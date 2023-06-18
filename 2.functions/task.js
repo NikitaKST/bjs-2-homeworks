f26f79ad66dbba9456ca2e9ac830803463b55c71
NikitaKST <8172535@gmail.com>
2023-06-18 15:03:20 +0500

    Function task 1 complete

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,27 @@
+//Задача 1
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
+
+//Задача 2
 
 function summElementsWorker(...arr) {
 
@@ -18,6 +38,8 @@ function differenceEvenOddWorker(...arr) {
 function averageEvenElementsWorker(...arr) {
 
 }
+
+//Задача 3
 
 function makeWork (arrOfArr, func) {
 
