1adb6fc1a7c47802a6913d04abe4f19ab0c76e72
NikitaKST <8172535@gmail.com>
2023-06-18 16:06:46 +0500

    Function task 2 complete

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,4 @@
-//Задача 1
+// //Задача 1
 function getArrayParams(...arr) {
   let min = Infinity;
   let max = -Infinity;
@@ -21,21 +21,101 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: avg };
 }
 
-//Задача 2
+console.log(getArrayParams(-9, 5, 8));
+
+/*
+function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((acc, curr) => acc + curr, 0);
+  let avg = Number((sum / arr.length).toFixed(2));
+
+  return { min, max, avg };
+}
+
+console.log(getArrayParams(-9, 5, 8));
+*/
+
+//Задача 2.1
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((acc, curr) => acc + curr, 0);
+  
+  return sum;
 }
 
+//Задача 2.2
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let difference = max - min;
+
+  return difference;
 }
 
+//Задача 2.3
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0; // Сумма четных элементов
+  let sumOddElement = 0; // Сумма нечетных элементов
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i]; 
+    } else {
+      sumOddElement += arr[i]; 
+    }
+  }
+
+  return sumEvenElement - sumOddElement; 
 }
 
+/*function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let sumEvenElement = 0; // Сумма четных элементов
+  let sumOddElement = 0; // Сумма нечетных элементов
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? (sumEvenElement += arr[i]) : (sumOddElement += arr[i]);
+  }
+
+  return sumEvenElement - sumOddElement - ; // Возвращаем разницу между суммой нечетных и суммой четных элементов
+}
+*/
+
+//Задача 2.4
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement +=1; 
+    }
+  }
+
+  return sumEvenElement / countEvenElement; 
 
 }
 
